b1778baf9b6354ba67fd8bfb61d49dff86ca43ff
George <71434803+george-swift@users.noreply.github.com>
2025-03-12 07:24:57 +0100

    Update entry point with blueprints and logger

--- app.py
+++ app.py
@@ -1,29 +1,13 @@
+import logging
 from chalice import Chalice
+from chalicelib.settings import settings
+from chalicelib.auth_challenges import auth_challenges
+from chalicelib.auth import auth
 
-app = Chalice(app_name='expenser-auth')
+app = Chalice(app_name=settings.cognito_app_client_name)
 
+app.log.setLevel(logging.INFO)
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+app.register_blueprint(auth)
 
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+app.register_blueprint(auth_challenges)
